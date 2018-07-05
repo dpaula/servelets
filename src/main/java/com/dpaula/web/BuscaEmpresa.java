9826dc503aec156c537730f8652b1582e1e1cade
dpaula <fernando.dpaula@gmail.com>
2018-07-04 23:27:15 -0300

    criando o método doGet para enviar as respostas para a servlet

--- src/main/java/com/dpaula/web/BuscaEmpresa.java
+++ src/main/java/com/dpaula/web/BuscaEmpresa.java
@@ -1,7 +1,23 @@
 package com.dpaula.web;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 public class BuscaEmpresa extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// rep = requisição = as informações que o cliente manda
+		// resp = resposta = as informações que vou enviar para o cliente
+		PrintWriter writer = resp.getWriter();
+		writer.println("<html><body>");
+		writer.println("Resultado da busca: <br/>");
+		writer.println("</body></html>");
+	}
 
 }

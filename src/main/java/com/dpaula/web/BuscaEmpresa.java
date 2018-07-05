e010ccfc293e255df0e1831eda512e37d95805b8
dpaula <fernando.dpaula@gmail.com>
2018-07-04 23:38:49 -0300

    trazendo uma lista na resposta do servlet

--- src/main/java/com/dpaula/web/BuscaEmpresa.java
+++ src/main/java/com/dpaula/web/BuscaEmpresa.java
@@ -2,12 +2,16 @@ package com.dpaula.web;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Collection;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import br.com.dpaula.Empresa;
+import br.com.dpaula.dao.EmpresaDAO;
 
 @WebServlet(urlPatterns = "/busca")
 public class BuscaEmpresa extends HttpServlet {
@@ -19,6 +23,20 @@ public class BuscaEmpresa extends HttpServlet {
 		PrintWriter writer = resp.getWriter();
 		writer.println("<html><body>");
 		writer.println("Resultado da busca: <br/>");
+
+		String filtro = "doce";
+
+		Collection<Empresa> empresas = new EmpresaDAO().buscaPorSimilaridade(filtro);
+		writer.println("<ul>");
+
+		for (Empresa empresa : empresas) {
+			writer.println("<li>");
+			writer.println(empresa.getId() + ": " + empresa.getNome());
+			writer.println("</li>");
+		}
+
+		writer.println("</ul>");
+
 		writer.println("</body></html>");
 	}
 

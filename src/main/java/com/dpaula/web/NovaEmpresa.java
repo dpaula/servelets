5ae2d6c05e965a2a13ed13ed7292e57c1703528e
dpaula <fernando.dpaula@gmail.com>
2018-07-05 21:45:02 -0300

    incluido novo servlet novaEmpresa para cadastro de empresas

---
+++ src/main/java/com/dpaula/web/NovaEmpresa.java
@@ -0,0 +1,32 @@
+package com.dpaula.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import br.com.dpaula.Empresa;
+import br.com.dpaula.dao.EmpresaDAO;
+
+@WebServlet(urlPatterns = "/novaEmpresa")
+public class NovaEmpresa extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		// http://localhost:8080/servlets/novaEmpresa?nome=Philips
+
+		String nome = req.getParameter("nome");
+		Empresa empresa = new Empresa(nome);
+
+		new EmpresaDAO().adiciona(empresa);
+
+		PrintWriter writer = resp.getWriter();
+		writer.println("<html><body>Empresa " + empresa.getNome() + " cadastrada com sucesso!</body></html>");
+	}
+
+}
